4373b0f371dbacb3dabd7d773453e08cca998688
Sprax Lines <sprax@talla.com>
2018-03-14 01:05:56 -0400

    explicit type names

--- example/example/main.cpp
+++ example/example/main.cpp
@@ -106,41 +106,41 @@ int main(int argc, const char * argv[]) {
     const char *cstr = "dragoon";
     string sstr = cstr;
 
-    unordered_set<const char *> cwords;
-    cwords.insert("ago");
-    cwords.insert("drag");
-    cwords.insert(cstr);
-    cwords.insert("dragoons");
-    cwords.insert("go");
-    cwords.insert("goo");
-    cwords.insert("goon");
-    cwords.insert("on");
-    cwords.insert("rag");
+    unordered_set<const char *> const_pcz_words;
+    const_pcz_words.insert("ago");
+    const_pcz_words.insert("drag");
+    const_pcz_words.insert(cstr);
+    const_pcz_words.insert("dragoons");
+    const_pcz_words.insert("go");
+    const_pcz_words.insert("goo");
+    const_pcz_words.insert("goon");
+    const_pcz_words.insert("on");
+    const_pcz_words.insert("rag");
 
-    unordered_set<const char *, hashChrStr, eqCharStrCmp> twords;
-    for (const auto& pword: cwords) {                                   // newish range-based for loop
-        twords.insert(pword);
+    unordered_set<const char *, hashChrStr, eqCharStrCmp> template_type_words;
+    for (const auto& pword: const_pcz_words) {                                   // newish range-based for loop
+        template_type_words.insert(pword);
     }
 
-    unordered_set<string> swords;
-    for (auto itr = twords.cbegin(); itr != twords.cend(); ++itr) {     // traditional iterator-based loop
-        swords.insert(*itr);
+    unordered_set<string> std_string_words;
+    for (auto itr = template_type_words.cbegin(); itr != template_type_words.cend(); ++itr) {     // traditional iterator-based loop
+        std_string_words.insert(*itr);
     }
 
     cout << "chars strncpy: " << strncpy(buffer, &cstr[2], 3) << endl;
-    cout << "Is string literal 'ago' in cwords? " << cwords.count("ago") << endl;
+    cout << "Is string literal 'ago' in const_pcz_words? " << const_pcz_words.count("ago") << endl;
     char ago_a[] = { 'a', 'g', 'o', '\0' };
-    cout << "Is separate array 'ago' in cwords? " << cwords.count(ago_a) << endl;
+    cout << "Is separate array 'ago' in const_pcz_words? " << const_pcz_words.count(ago_a) << endl;
 
     cout << "chars strncpy: " << strncpy(buffer, &cstr[2], 3) << endl;
-    cout << "Is string literal 'ago' in twords? " << twords.count("ago") << endl;
-    cout << "Is separate array 'ago' in twords? " << twords.count(ago_a) << endl;
+    cout << "Is string literal 'ago' in template_type_words? " << template_type_words.count("ago") << endl;
+    cout << "Is separate array 'ago' in template_type_words? " << template_type_words.count(ago_a) << endl;
 
     // TODO: Can we extract a read-only range without creating a new string?
     cout << "string.substr: " << sstr.substr(2, 3) << endl;
-    cout << "Is string literal 'ago' in swords? " << swords.count("ago") << endl;
+    cout << "Is string literal 'ago' in std_string_words? " << std_string_words.count("ago") << endl;
     string ago_s = ago_a;
-    cout << "Is another string 'ago' in swords? " << swords.count(ago_s) << endl;
+    cout << "Is another string 'ago' in std_string_words? " << std_string_words.count(ago_s) << endl;
 
 
     
